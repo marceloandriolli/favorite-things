fe8e031b8a69a9048d71a10bd4d663659ac16726
marcelo andriolli <marcelorsa@gmail.com>
2019-09-05 23:53:24 -0300

    added nav bar component

---
+++ src/components/NavBar.vue
@@ -0,0 +1,13 @@
+<template>
+  <nav class="blue lighten-2 " role="navigation">
+    <div class="nav-wrapper">
+      <a href="#" class="brand-logo center">Favorite Things</a>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+}
+</script>
