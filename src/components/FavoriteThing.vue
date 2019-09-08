c9555dea246164e4e4352636d0f408053b40aa44
marcelo andriolli <marcelorsa@gmail.com>
2019-09-08 01:39:24 -0300

    added favorite thing component

---
+++ src/components/FavoriteThing.vue
@@ -0,0 +1,16 @@
+<template>
+    <li class="collection-item avatar">
+        <span class="title"><b>Title</b>: {{ title }}</span>
+        <p><b>Description</b>: {{ description }}</p>
+        <p><b>Category</b>: {{ category }}</p>
+        <a href="#!" class="secondary-content btn-floating btn-small blue lighten-2"><i class="material-icons">delete</i></a>
+    </li>
+</template>
+
+
+<script>
+export default {
+    name: 'FavoriteThing',
+    props: ['title', 'description', 'category']
+}
+</script>
