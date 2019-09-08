0e3d43f653ac7115342b0f976d19c099a91a84c2
marcelo andriolli <marcelorsa@gmail.com>
2019-09-08 12:59:21 -0300

    added delete favorite thing item from list

--- src/components/FavoriteThing.vue
+++ src/components/FavoriteThing.vue
@@ -3,7 +3,7 @@
         <span class="title"><b>Title</b>: {{ title }}</span>
         <p><b>Description</b>: {{ description }}</p>
         <p><b>Category</b>: {{ category }}</p>
-        <a href="#!" class="secondary-content btn-floating btn-small blue lighten-2"><i class="material-icons">delete</i></a>
+        <a v-on:click="deleteClicked" class="secondary-content btn-floating btn-small blue lighten-2"><i class="material-icons">delete</i></a>
     </li>
 </template>
 
@@ -11,6 +11,11 @@
 <script>
 export default {
     name: 'FavoriteThing',
-    props: ['title', 'description', 'category']
+    props: ['title', 'description', 'category'],
+    methods: {
+        deleteClicked(){
+            this.$emit('deleteclicked', this.title);
+        }
+    }
 }
 </script>
--- src/components/FavoriteThingsList.vue
+++ src/components/FavoriteThingsList.vue
@@ -3,6 +3,7 @@
         <div class="col s8 offset-s2 items">
             <ul v-if="ThingsList.length > 0" class="collection">
                 <FavoriteThing v-for="item in ThingsList"
+                               v-on:deleteclicked="deleteFavorite($event)"
                                :title="item.title"
                                :description="item.description"
                                :category="item.category"/>
@@ -15,13 +16,19 @@
 import FavoriteThing from './FavoriteThing';
 export default {
     name: 'FavoriteThingsList',
+    components: {
+        FavoriteThing,
+    },
     data(){
         return {
             ThingsList: []
         }
     },
-    components: {
-        FavoriteThing,
+    methods:{
+        deleteFavorite(title){
+            const indexOfFavoriteThings = this.ThingsList.indexOf(name);
+            this.ThingsList.splice(indexOfFavoriteThings, 1);
+        }
     }
 }
 </script>
