eb51df4536248f25a54cedbb590e759a2cf426dd
marcelo andriolli <marcelorsa@gmail.com>
2019-09-08 02:06:50 -0300

    added favorite things list component

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,22 @@
   <div id="app">
     <Navbar />
       <div class="container">
-
+          <div class="section">
+              <FavoriteThingsList/>
+          </div>
       </div>
   </div>
 </template>
 
 <script>
-import Navbar from './components/NavBar'
+import Navbar from './components/NavBar';
+import FavoriteThingsList from './components/FavoriteThingsList';
+
 export default {
   name: 'app',
   components: {
       Navbar,
+      FavoriteThingsList
 
   }
 }
---
+++ src/components/FavoriteThingsList.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="row">
+        <div class="col s8 offset-s2 items">
+            <ul v-if="ThingsList.length > 0" class="collection">
+                <FavoriteThing v-for="item in ThingsList"
+                               :title="item.title"
+                               :description="item.description"
+                               :category="item.category"/>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+import FavoriteThing from './FavoriteThing';
+export default {
+    name: 'FavoriteThingsList',
+    data(){
+        return {
+            ThingsList: []
+        }
+    },
+    components: {
+        FavoriteThing,
+    }
+}
+</script>
